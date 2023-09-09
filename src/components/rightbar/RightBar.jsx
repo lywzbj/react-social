131ec5f81373e52003c8dd11d941fcd328478da8
luoyu <18715838915@163.com>
2023-09-09 22:51:38 +0800

    右侧布局编写

--- src/components/rightbar/RightBar.jsx
+++ src/components/rightbar/RightBar.jsx
@@ -6,7 +6,70 @@ export default function RightBar() {
     return (
 
         <div className='rightbar'>
-            rightbar
+            <div className="rightbarWrapper">
+                <div className="rightbarBirth">
+                    <img className='birthDayImg' src='/assets/gift.png' />
+                    <span><b>Pola Foster</b> and <b>3 other friends</b>have a birthday today.</span>
+
+
+                </div>
+
+                <img className='rightbarAd' src='/assets/ad.png' />
+
+
+                <div className="onlineFriends">
+                    <h4>Online Friends</h4>
+                    <ul className='rightbarOnlineFriendList'>
+                        <li className='onlineFriendItem'>
+                            <div className="rightFriendItemContainer">
+                                <img src='/assets/person/5.jpeg' className='onlineFriendImg' />
+                                <span className="rightbarOnlinePoint"></span>
+                                <span className="onlienFriendUserName">John Carter</span>
+                            </div>
+                        </li>
+                        <li className='onlineFriendItem'>
+                            <div className="rightFriendItemContainer">
+                                <img src='/assets/person/5.jpeg' className='onlineFriendImg' />
+                                <span className="rightbarOnlinePoint"></span>
+                                <span className="onlienFriendUserName">John Carter</span>
+                            </div>
+                        </li>
+                        <li className='onlineFriendItem'>
+                            <div className="rightFriendItemContainer">
+                                <img src='/assets/person/5.jpeg' className='onlineFriendImg' />
+                                <span className="rightbarOnlinePoint"></span>
+                                <span className="onlienFriendUserName">John Carter</span>
+                            </div>
+                        </li>
+                        <li className='onlineFriendItem'>
+                            <div className="rightFriendItemContainer">
+                                <img src='/assets/person/5.jpeg' className='onlineFriendImg' />
+                                <span className="rightbarOnlinePoint"></span>
+                                <span className="onlienFriendUserName">John Carter</span>
+                            </div>
+                        </li>
+                        <li className='onlineFriendItem'>
+                            <div className="rightFriendItemContainer">
+                                <img src='/assets/person/5.jpeg' className='onlineFriendImg' />
+                                <span className="rightbarOnlinePoint"></span>
+                                <span className="onlienFriendUserName">John Carter</span>
+                            </div>
+                        </li>
+                        <li className='onlineFriendItem'>
+                            <div className="rightFriendItemContainer">
+                                <img src='/assets/person/5.jpeg' className='onlineFriendImg' />
+                                <span className="rightbarOnlinePoint"></span>
+                                <span className="onlienFriendUserName">John Carter</span>
+                            </div>
+                        </li>
+                    </ul>
+
+
+
+
+                </div>
+
+            </div>
         </div>
 
     )
